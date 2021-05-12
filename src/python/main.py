4b0b065ed03c6db9b856640f3f1a3e494d6c8295
aleksmllr <aleksmllr@gmail.com>
2021-05-11 20:14:54 -0700

    Added a yaml file to store information needed to login to my Anytime Fitness Account as well as the necesary code to lparse the yaml file

--- src/python/main.py
+++ src/python/main.py
@@ -1,12 +1,27 @@
+import yaml
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# Goal: Reserve 9:00am Gym Slot three days from the current day
+
+# Need a function that acquires the correct day based on current day
+
+# Need a function to select the 9:00am time slot
+
+def getDay():
+    pass
+
+def reserve(time):
+    pass
+
+# Get log-in information from the YAML file
+credentials = open("credentials/creds.yaml")
+parsed_yaml = yaml.load(credentials, Loader=yaml.FullLoader)
+key = parsed_yaml["gymfo"]["key"]
+last_name = parsed_yaml["gymfo"]["last_name"]
+
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver.get("https://reserve.anytimefitness.com/clubs/4812")
+
 driver.quit()
