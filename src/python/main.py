be3cef029a36dcd1b6e27170b89fc3858d74f22b
aleksmllr <aleksmllr@gmail.com>
2021-05-13 01:02:01 -0700

    Learned how to step through web pages and find/interact with html elements, now at the point whereI'm logged in to my Anytime Fitness Profile and can view the Reservation Schedule for the next three days including current day i.e Monday-Wednesday, Tuesday-Thursday. etc.

--- src/python/main.py
+++ src/python/main.py
@@ -1,8 +1,11 @@
-import yaml
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+import yaml
+import time
 # Goal: Reserve 9:00am Gym Slot three days from the current day
 
 # Need a function that acquires the correct day based on current day
@@ -24,4 +27,42 @@ last_name = parsed_yaml["gymfo"]["last_name"]
 driver = webdriver.Chrome()
 driver.get("https://reserve.anytimefitness.com/clubs/4812")
 
-driver.quit()
+try:
+    # Wait until login button has been located
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "user-action"))
+    )
+    # Click on Log-In Button
+    element.click()
+    # Wait until Log-In page has loaded and the key fob text bar has been located
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "keyfob"))
+    )
+    # send the Key Fob number to the appropriate text bar
+    element.clear()
+    element.send_keys(key)
+    # find the last name text box
+    element = driver.find_element_by_name("lastName")
+    element.clear()
+    # send last name to appropriate text bar
+    element.send_keys(last_name)
+    # find the "look me up!" button and click it
+    element = driver.find_element_by_xpath("/html/body/main/div/section[1]/form/section[2]/div[3]/button").click()
+
+    element = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "res-content"))
+    )
+    days = element.find_elements_by_class_name("res-day")
+
+    for day in days:
+        print(day)
+
+    time.sleep(5)
+
+    
+
+finally:
+    driver.quit()
+
+
+
